66aae04ef1534fff9617eafddee23f689f231ab5
Darren Aquilina <darren.aquilina.a100446@mcast.edu.mt>
2018-07-16 20:51:46 +0200

    urls for deployment

--- src/app/invasions/invs.component.ts
+++ src/app/invasions/invs.component.ts
@@ -58,11 +58,11 @@ export class InvasionsComponent implements AfterViewInit {
 
   getImage(cogs_attacking){
     cogs_attacking = cogs_attacking === 'None' ? 'logo_icon' : cogs_attacking;
-    return '/assets/' + cogs_attacking + '.png';
+    return './assets/' + cogs_attacking + '.png';
   }
 
   getCogType(cogs_type){
-    return "url('/assets/" + cogs_type + ".png')";
+    return "url('./assets/" + cogs_type + ".png')";
   }
 
   getGradient(number){
@@ -71,7 +71,7 @@ export class InvasionsComponent implements AfterViewInit {
   }
 
   notify(district: IDistrict){
-    this.pushNotifications.create('Invasion Alert', {body: '\nAn Invasion has started in ' + district.name + '!\n', icon: '/assets/' + district.cogs_attacking + '.png'}).subscribe(
+    this.pushNotifications.create('Invasion Alert', {body: '\nAn Invasion has started in ' + district.name + '!\n', icon: './assets/' + district.cogs_attacking + '.png'}).subscribe(
       res => {
           if (res.event.type === 'click') {
               res.notification.close();
@@ -96,7 +96,7 @@ export class InvasionsComponent implements AfterViewInit {
         {
           if(this.lastRequestDown)
           {
-            this.pushNotifications.create('Server Status Alert', {body: '\nDistricts are back online.\nServers could possibly also be accessible.', icon: '/assets/logo_icon.png'}).subscribe(
+            this.pushNotifications.create('Server Status Alert', {body: '\nDistricts are back online.\nServers could possibly also be accessible.', icon: './assets/logo_icon.png'}).subscribe(
               res => {
                   if (res.event.type === 'click') {
                       res.notification.close();
